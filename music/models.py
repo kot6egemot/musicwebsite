1966bc891e4dc6738c006e56895d9431893ddf43
kot6egemot <ex-2k@yandex.ru>
2019-03-01 08:29:45 +0300

    refractorin code

--- music/models.py
+++ music/models.py
@@ -1,7 +1,7 @@
 import os
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+    # Create your models here.
 
 class Artist(models.Model):
     title = models.CharField(max_length=20)
